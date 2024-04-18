d41dea382142261ca610792f37fc671ca76932a2
JoseOsorio03 <167139921+JoseOsorio03@users.noreply.github.com>
2024-04-17 21:12:18 -0600

    Add files via upload
    Se realizan correciones en funciones _kbhith y _getch, como tambien se agrega switch case en el que se permite por medio de if else se logra la pantalla circular.

--- Proyecto Progra/main.cpp
+++ Proyecto Progra/main.cpp
@@ -7,7 +7,7 @@
 #define DERECHA 77
 #define IZQUIERDA 75
 
-void gotoxy(int x,int y){
+void gotoxy(int x, int y) {
     HANDLE hCon;
     hCon = GetStdHandle(STD_OUTPUT_HANDLE);
     COORD dwPos;
@@ -17,29 +17,39 @@ void gotoxy(int x,int y){
     SetConsoleCursorPosition(hCon, dwPos);
 }
 
-//void limites(){
- //   for(int i =2; i<78; i++){
-  //      gotoxy(i,3); print("%c",205);
-  //  }
-//}
-
-int main(){
-
-    int x=0,y=0;
-    gotoxy(x,y);printf("*");
+int main() {
+    int x = 0, y = 0;
+    gotoxy(x, y); printf("*");
 
     bool game_over = false;
-    while(!game_over){
-
-
-        if(kbhit()){
-            char tecla = getch();
-            gotoxy(x,y);printf(" ");
-            if(tecla == IZQUIERDA) x--;
-            if(tecla == DERECHA) x++;
-            if(tecla == ARRIBA) y--;
-            if(tecla == ABAJO) y++;
-            gotoxy(x,y); printf("*");
+    while (!game_over) {
+        if (_kbhit()) {
+            char tecla = _getch();
+            gotoxy(x, y); printf(" ");
+            switch (tecla) {
+                case IZQUIERDA:
+                    if (x > 0) x--;
+                    else x = 79; // Si llega al límite izquierdo, vuelve al límite derecho
+                    break;
+                case DERECHA:
+                    if (x < 79) x++;
+                    else x = 0; // Si llega al límite derecho, vuelve al límite izquierdo
+                    break;
+                case ARRIBA:
+                    if (y > 0) y--;
+                    else y = 34; // Si llega al límite superior, vuelve al límite inferior
+                    break;
+                case ABAJO:
+                    if (y < 34) y++;
+                    else y = 0; // Si llega al límite inferior, vuelve al límite superior
+                    break;
+                case 'q': // Tecla para salir del juego
+                    game_over = true;
+                    break;
+            }
+            if (!game_over) {
+                gotoxy(x, y); printf("*");
+            }
         }
     }
 
